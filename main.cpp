615e79d5d7552be4500acee40e5be44ad1b75ab5
Tayaba Kabir Richee <128034034+2021331074@users.noreply.github.com>
2023-12-28 22:46:52 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+char board[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+int main(){
+  for(int i=0; i<9; i++){
+    board draw();
+    move();
+    switching();
+  }
+  board draw();
+  cout<<"Upps no one wins! It's a draw! "<<endl;
+  return 0;  
+}
