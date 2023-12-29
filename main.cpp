619d276be6de1b9bfb4b918025f19a72911e0780
Tayaba Kabir Richee <128034034+2021331074@users.noreply.github.com>
2023-12-29 22:22:57 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+char currentPlayer = 'X';
+bool gameover = false;
+
+void boarddraw() {
+    cout << "TIC TAC TOE" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void move() {
+    int choice;
+    cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+    cin >> choice;
+
+    
+    int row = (choice - 1) / 3; 
+
+    int col = (choice - 1) % 3;
+
+    if (choice < 1 || choice > 9 || board[row][col] == 'X' || board[row][col] == 'O') {
+        cout << "Invalid move. Please choose an available spot" << endl;
+        move(); 
+    } 
+    else {
+        board[row][col] = currentPlayer;
+
+        if ((board[row][0] == currentPlayer && board[row][1] == currentPlayer && board[row][2] == currentPlayer) ||
+            (board[0][col] == currentPlayer && board[1][col] == currentPlayer && board[2][col] == currentPlayer) ||
+            (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+            (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+            boarddraw();
+            cout << "Player " << currentPlayer << " wins!" << endl;
+            gameover = true;
+        }
+    }
+}
+
+void switching() {
+    if (currentPlayer == 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+
+int main() {
+    while (!gameover) {
+        boarddraw();
+        move();
+        switching();
+    }
+    
+    
+    return 0;
+}
